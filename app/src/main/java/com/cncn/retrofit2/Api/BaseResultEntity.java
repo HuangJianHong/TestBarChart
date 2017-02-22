0e3af79c72155b503100bc621ed527a25e310526
Huangjh <Huangjh>
2017-02-22 16:08:58 +0800

    [FIX]修改Test依赖出错，更新RxJava 和 retrofit依赖

---
+++ app/src/main/java/com/cncn/retrofit2/Api/BaseResultEntity.java
@@ -0,0 +1,8 @@
+package com.cncn.retrofit2.Api;
+
+/**
+ * Created by T163 on 2017/2/22.
+ */
+
+public class BaseResultEntity {
+}
---
+++ app/src/main/java/com/cncn/retrofit2/Api/exception/RetryWhenNetworkException.java
@@ -0,0 +1,8 @@
+package com.cncn.retrofit2.Api.exception;
+
+/**
+ * Created by T163 on 2017/2/22.
+ */
+
+public class RetryWhenNetworkException {
+}
